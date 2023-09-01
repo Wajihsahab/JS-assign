11fa565a20ca0635adfdc068a6210af5c53afefe
Wajihsahab <117687362+Wajihsahab@users.noreply.github.com>
2023-09-01 21:26:13 +0500

    Update jsassign.js

---
+++ jsassign.js
@@ -0,0 +1,106 @@
+//   CHAPTER NO 1        
+//Q NO 1
+
+/*var greetingMessage = "hello frands chai peelo";
+alert(greetingMessage);*/
+
+//Q NO 2
+
+
+/*var message = "Error! Please enter a valid password";
+alert(message);*/
+
+//Q No 3
+
+/*var greetingMessage = "Welcome to js land..\n Happy coding!";
+alert(greetingMessage);*/
+
+//Q No 4.1
+
+/*var greetingMessage = "Welcome to JS Land...";
+alert(greetingMessage);*/
+
+//Q No 4.2 error
+
+/*function alertWithCheckbox() {
+  
+  var message = document.createElement('p');
+  message.textContent ='Hello Coder';
+
+  var checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  
+  var label =document.createElement('label');
+  label.textContent = 'Prevent this command dialog';
+
+  var okButton = document.createElement('button');
+  okButton.onclick = function(){
+    var isChecked = checkbox.checked;
+    if(isChecked){
+      console.log('Command dialog prevented.');
+    }
+    else{
+      alert('command dialog shown.');
+    }
+    alert(message, checkbox, label)
+  }*/
+
+//Q No 5
+
+/*var greetingMessage = "Hello..I can run JS through my web browser's console ";
+alert(greetingMessage);*/
+ 
+// CHAPTER NO 2 START
+                               
+//Q No 1
+
+//var username;
+
+//Q No 2
+
+//var myName ="Meerza M. Wajih";
+
+//Q No 3
+
+//var message="Hello World";
+//alert(message);
+
+//Q No 4.1
+
+//var greetingMessage ="Dhone Doe";
+//alert(greetingMessage);
+
+//Q No 4.2
+
+/* var greetingMessage ="15 Years old";
+alert(greetingMessage);*/
+                                   
+//Q No 4.3
+
+/*var greetingMessage ="Certified Mobile Application Development";
+alert(greetingMessage);*/
+                              
+//Q No 5
+
+/*var greetingMessage ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(greetingMessage);*/
+
+//Q No 6
+
+/*var greetingMessage ="email adress ak5589667@gmail.com";
+alert(greetingMessage);*/
+
+//Q No 7
+
+/*var greetingMessage ="i am trying to learn form the BOOk A Smarter\nWay to learn javascript";
+alert(greetingMessage);*/
+
+//Q No 8
+
+/*var greetingMessage ="Yah! i can write HTML content through javasscript";
+alert(greetingMessage);*/
+
+//Q No 9
+
+/*var greetingMessage ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(greetingMessage);*/
